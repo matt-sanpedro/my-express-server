40e8d5cb4118d1b21806a9206081d7f11a5dac47
matt-sanpedro <mattsanpedro76@gmail.com>
2020-11-02 00:16:03 -0800

    added get method

--- server.js
+++ server.js
@@ -1,6 +1,21 @@
+// jshint esversion:6
+
 const express = require('express');
 
 // function that represents the express module, bind that to a word "app"
 const app = express();
 
-app.listen(3000);
+// what happens when client makes get request
+app.get('/', function(req, res) {
+  // console.log(request);
+  res.send("<h1>Hello Batman</h1>")
+})
+
+app.get('/batman', function(req, res) {
+  // console.log(request);
+  res.send("<h1>Hello Robin</h1>")
+})
+
+app.listen(3000, function() {
+  console.log('Server started on port 3000');
+});
